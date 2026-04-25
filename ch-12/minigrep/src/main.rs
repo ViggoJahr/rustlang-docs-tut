8538e6367a442d4ca331d01811cdc6f5ba5c7d44
Viggo Jahr <viggo.jahr@gmail.com>
2026-04-25 13:38:40 +0200

    a minigrep writing from a file and printing in a terminal, can be case sensitive and Insensitive

--- ch-12/minigrep/src/main.rs
+++ ch-12/minigrep/src/main.rs
@@ -1,16 +1,58 @@
-use std::{env, fs}
+use std::{env, process, fs, error::Error};
+use minigrep::{search, search_case_insensitive};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err|{
+        // Anonymous function with err as passed argument
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    //println!("Searching for {}", config.query);
+    //println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-
-    println!("with text: \n{contents}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+         if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+
+        Ok(Config { query, file_path, ignore_case})
+    }
+}
+
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(()) // Only interested in side-effects.
+}
+
