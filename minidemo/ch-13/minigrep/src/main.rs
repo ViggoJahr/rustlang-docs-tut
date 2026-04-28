dea6647d337367d557c657d177c75f2b4a07c7d2
Viggo Jahr <viggo.jahr@gmail.com>
2026-04-28 13:30:14 +0200

    refactored minigrep with iterators and closures

--- minidemo/ch-13/minigrep/src/main.rs
+++ minidemo/ch-13/minigrep/src/main.rs
@@ -4,14 +4,10 @@ use minigrep::{search, search_case_insensitive};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err|{
-        // Anonymous function with err as passed argument
+    let config = Config::build(env::args()).unwrap_or_else(|err|{
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
-
-    //println!("Searching for {}", config.query);
-    //println!("In file {}", config.file_path);
 
     if let Err(e) = run(config) {
         eprintln!("Application error: {e}");
@@ -26,16 +22,30 @@ pub struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-         if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    fn build(
+        mut args: impl Iterator<Item = String>, 
+    ) -> Result<Config, &'static str> {
+         args.next(); //skipping first arg (name of the program)
+        
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
 
-        Ok(Config { query, file_path, ignore_case})
+        Ok(Config { 
+            query, 
+            file_path, 
+            ignore_case
+        })
     }
 }
 
