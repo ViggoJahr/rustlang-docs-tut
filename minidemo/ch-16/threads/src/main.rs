ce41049773a34c71fe4a6597d7f1911f15fc017d
Viggo Jahr <viggo.jahr@gmail.com>
2026-04-29 17:31:40 +0200

    learning about threads and how they can be used

---
+++ minidemo/ch-16/threads/src/main.rs
@@ -0,0 +1,27 @@
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("Hi number {i} from the spawned thread");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..6 {
+        println!("Hi number {i} from the main thread");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Move necessary to give the closure ownership of v. Otherwise v might drop before used.
+    thread::spawn(move || { 
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle.join().unwrap();
+
+}
